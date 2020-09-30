048f3416882a402450982ab9f2c8a59b0aa6bf4e
ruby910122 <71751285+ruby910122@users.noreply.github.com>
2020-09-30 15:16:58 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+let GPA = 0
+input.onGesture(Gesture.Shake, function () {
+    GPA = randint(80, 100)
+    if (GPA < 84) {
+        basic.showNumber(randint(80, 84))
+        basic.showString("A-")
+    } else if (GPA < 89) {
+        basic.showNumber(randint(85, 89))
+        basic.showString("A")
+    } else if (GPA < 100) {
+        basic.showNumber(randint(90, 100))
+        basic.showString("A+")
+    } else {
+        basic.showString("A-")
+    }
 })
